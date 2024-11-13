d5abb2f533276573d75e620d99b34f77cc572b92
Pedro Wrublak <pedrovili.w@gmail.com>
2024-11-13 17:31:47 -0300

    criando botão de apagar

--- index.js
+++ index.js
@@ -19,14 +19,34 @@ function limpaInput() {
     inputTarefa.focus();
 }
 
+function criaBotaoApagar(li) {
+    li.innerHTML += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'Apagar essa tarefa');
+    li.appendChild(botaoApagar);
+}
+
 function criaTarefa(textoInput) {
     const li = criaLi();
     li.innerHTML = textoInput;
     tarefas.appendChild(li);
     limpaInput();
+    criaBotaoApagar(li)
 }
 
 btnTarefa.addEventListener('click', function() {
     if (!inputTarefa.value) return;
     criaTarefa(inputTarefa.value);
 });
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+    
+    if (el.classList.contains('apagar')) {
+        
+    }
+});
+
+//VER E COMENTAR O CÓDIGO
